2670c455169d9cb48abfdce33a81d3cd624615f2
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2023-02-01 20:25:41 -0500

    Create a remote backend

---
+++ remote_backend/main.tf
@@ -0,0 +1,18 @@
+# Create the S3 Bucket for the remote states storage
+resource "aws_s3_bucket" "terraform_remote_state" {
+  bucket = "wordpress-aws-2023"
+}
+
+# Create a Dynamodb table to Lock the state file
+resource "aws_dynamodb_table" "terraform_remote_state" {
+  name           = "wordpress-remote-db"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
